5721d9c4aae34763fc90d18f18bcc08d0fea17c3
fukai0116 <134059797+fukai0116@users.noreply.github.com>
2024-02-02 04:20:43 +0900

    pp

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, ListGroup, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 import './EventDetails.css'; 
 
 
